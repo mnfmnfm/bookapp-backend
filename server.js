ff860c1630e2283014b93fae1fcd00583268cd2d
mnfmnfm <mnfmnfm@gmail.com>
2018-05-21 12:27:48 -0700

    configure db and cors

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 'use strict';
 
+const cors = require('cors');
 const express = require('express');
+const pg = require('pg');
+
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.log(err));
+
 const app = express();
+app.use(cors());
 
 app.get('/books', (req, res) => {
   res.send('books are great');
+});
+
+app.use('*', (req, res) => {
+  res.status(404).send('resource not found');
 });
 
 const PORT = process.env.PORT || 3000;
